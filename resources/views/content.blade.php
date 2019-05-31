29e9fe3441f38deb79306d4cb6ef2b1c3cfcb258
Andreaf2395 <andreaf2395@gmail.com>
2019-05-31 19:29:39 +0530

    added navbar

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main_layout')
 
+
 @section('content')
-	i am content
+	
+	@include('includes.sidebar')
+
+
+	content goes here
 @stop
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,3 +1,27 @@
-<div>
-	sidebar
-</div>
+<ul id="slide-out" class="sidenav sidenav-fixed">
+    <li><div class="user-view">
+      <div class="background">
+        <!-- <img src="images/office.jpg"> -->
+      </div>
+      <!-- <a href="#user"><img class="circle" src="images/yuna.jpg"></a> -->
+      <a href="#name"><span class="white-text name">John Doe</span></a>
+      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a class="subheader">Courses</a></li>
+    <li><a href="#!" >Task0</a></li>
+    <li><a href="#!" >Task1</a></li>
+    <li><a href="#!" >Task2</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Leaderboard</a></li>
+    <li><a class="waves-effect" href="#!">Discussion</a></li>
+    <li><a class="waves-effect" href="#!">Progress Stats</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+  <script>
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  </script>
