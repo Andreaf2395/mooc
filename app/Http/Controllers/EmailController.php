982070878fa9e1d90c94b0400d0d9beba233eff3
shubhamhegde <shubhamhegde6@gmail.com>
2019-06-24 18:32:00 +0530

    mail_flag checking added in EmailController

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -22,6 +22,7 @@ class EmailController extends Controller
     	$two_days_later=$current->addDays(2);
 
 
+    	//find all the team_ids that have submitted the particular task whose deadline is two days away
     	$teams_submitted=DB::table('team_course_maps')
     		->join('task_schedules','team_course_maps.c_id','=','task_schedules.c_id')
     		->where('task_schedules.end_date','=',$two_days_later)
@@ -31,21 +32,28 @@ class EmailController extends Controller
             ->get();
         //dd($teams_submitted);
 
-
+        //finding the team_ids that haven't submitted their course's particular task
         $teams_not_submitted=team_course_map::whereNotIn('team_id',$teams_submitted->pluck('team_id'))->whereIn('c_id',$teams_submitted->pluck('c_id'))->pluck('team_id');
         //dd($teams_not_submitted);
 
-        $receiversAddress=team_member_detail::whereIn('team_id',$teams_not_submitted)->pluck('email');
-        dd($receiversAddress);
-
+        //content to be displayed in the email
     	$content = [
     		'title'=> 'Reminder to submit task', 
     		'body'=> 'This is to inform you that you only have 2 more days to submit your task(MCQ and/or assignment)',
     		'button' => 'Click here'
     		];
 
+		foreach($teams_not_submitted as $team){
+			
+			//checking for the mail_flag and sending the mail accordingly team wise in a queue
+			if(!team_course_map::where('team_id',$team)->value('mail_flag')){
 
-    	Mail::to($receiversAddress)->queue(new submissionMail($content));
+				$receiversAddress=team_member_detail::where('team_id',$team)->pluck('email');
+				echo $receiversAddress;
+	    		Mail::to($receiversAddress)->queue(new submissionMail($content));
+	    		team_course_map::where('team_id',$team)->update(['mail_flag'=>1]);
+	    	}
+		}
 
 
     	dd('mail sent successfully');
